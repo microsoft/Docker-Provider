f7f6e3d0e5f4d9de7cd347572eb81b23993b52c7
rashmy <rashmy@RASHMY-PC2>
2018-10-22 14:04:46 -0700

    changes

---
+++ source/code/plugin/DockerApiClient.rb
@@ -0,0 +1,18 @@
+#!/usr/local/bin/ruby
+# frozen_string_literal: true
+
+class DockerApiClient
+
+    require 'socket'
+    require 'json'
+    require_relative 'oms_common'
+
+    @@SocketPath = "/var/run/docker.sock"
+    @@ChunkSize = 4096
+    
+
+
+
+end
+
+        
