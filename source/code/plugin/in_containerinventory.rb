2c4ddff5c0504dab90b3f32e75ed6c2ec02c75aa
rashmy <rashmy@RASHMY-PC2>
2018-10-31 18:55:06 -0700

    adding collection time

--- source/code/plugin/in_containerinventory.rb
+++ source/code/plugin/in_containerinventory.rb
@@ -183,6 +183,7 @@ module Fluent
           nameMap = DockerApiClient.getImageIdMap
           containerIds.each do |containerId|
             inspectedContainer = {}
+            inspectedContainer['CollectionTime'] = batchTime #This is the time that is mapped to become TimeGenerated
             inspectedContainer = inspectContainer(containerId, nameMap)
             inspectedContainer['Computer'] = hostname
             containerInventory.push inspectedContainer
