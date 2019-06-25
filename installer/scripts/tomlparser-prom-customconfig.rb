d9f641c6b08657125eff76c7470a839d3e54dd70
rashmy <rashmy@RASHMY-PC2>
2019-06-25 16:07:20 -0700

    formatting changes

--- installer/scripts/tomlparser-prom-customconfig.rb
+++ installer/scripts/tomlparser-prom-customconfig.rb
@@ -56,12 +56,12 @@ end
 
 # Use the ruby structure created after config parsing to set the right values to be used as environment variables
 def populateSettingValuesFromConfigMap(parsedConfig)
-puts "****************Start Prometheus Config Processing********************"
+  puts "****************Start Prometheus Config Processing********************"
   # Checking to see if this is the daemonset or replicaset to parse config accordingly
   controller = ENV["CONTROLLER_TYPE"]
   if !controller.nil?
-      if !parsedConfig.nil? && !parsedConfig[:prometheus_data_collection_settings].nil? 
-        if controller.casecmp(@replicaset) == 0 && !parsedConfig[:prometheus_data_collection_settings][:cluster].nil?
+    if !parsedConfig.nil? && !parsedConfig[:prometheus_data_collection_settings].nil?
+      if controller.casecmp(@replicaset) == 0 && !parsedConfig[:prometheus_data_collection_settings][:cluster].nil?
         #Get prometheus replicaset custom config settings
         begin
           interval = parsedConfig[:prometheus_data_collection_settings][:cluster][:interval]
@@ -71,36 +71,30 @@ puts "****************Start Prometheus Config Processing********************"
           kubernetesServices = parsedConfig[:prometheus_data_collection_settings][:cluster][:kubernetes_services]
           monitorKubernetesPods = parsedConfig[:prometheus_data_collection_settings][:cluster][:monitor_kubernetes_pods]
 
-          if checkForType(interval, String) 
-            && checkForTypeArray(fieldPass, String)
-            && checkForTypeArray(fieldDrop, String)
-            && checkForTypeArray(kubernetesServices, String)
-            && checkForTypeArray(urls, String)
-            && checkForType(monitorKubernetesPods, Boolean)
+          if checkForType(interval, String) && checkForTypeArray(fieldPass, String) && checkForTypeArray(fieldDrop, String) && checkForTypeArray(kubernetesServices, String) && checkForTypeArray(urls, String) && checkForType(monitorKubernetesPods, Boolean)
 
             # Write the settings to file, so that they can be set as environment variables
-file = File.open("prom_config_env_var", "w")
+            file = File.open("prom_config_env_var", "w")
 
-if !file.nil?
-  file.write("export AZMON_RS_PROM_INTERVAL=#{interval}\n")
-  file.write("export AZMON_RS_PROM_FIELDPASS=\"#{fieldPass}\"\n")
-  file.write("export AZMON_RS_PROM_FIELDDROP=#{fieldDrop}\n")
-  file.write("export AZMON_RS_PROM_K8S_SERVICES=#{kubernetesServices}\n")
-  file.write("export AZMON_RS_PROM_URLS=#{urls}\n")
-  file.write("export AZMON_RS_PROM_MONITOR_PODS=#{monitorKubernetesPods}\n")
-  # Close file after writing all environment variables
-  file.close
-  puts "****************End Prometheus Config Processing********************"
-else
-  puts "config::error::Exception while opening file for writing prometheus  replicaset config environment variables"
-  puts "****************End Prometheus Config Processing********************"
-end
-
+            if !file.nil?
+              file.write("export AZMON_RS_PROM_INTERVAL=#{interval}\n")
+              file.write("export AZMON_RS_PROM_FIELDPASS=\"#{fieldPass}\"\n")
+              file.write("export AZMON_RS_PROM_FIELDDROP=#{fieldDrop}\n")
+              file.write("export AZMON_RS_PROM_K8S_SERVICES=#{kubernetesServices}\n")
+              file.write("export AZMON_RS_PROM_URLS=#{urls}\n")
+              file.write("export AZMON_RS_PROM_MONITOR_PODS=#{monitorKubernetesPods}\n")
+              # Close file after writing all environment variables
+              file.close
+              puts "****************End Prometheus Config Processing********************"
+            else
+              puts "config::error::Exception while opening file for writing prometheus  replicaset config environment variables"
+              puts "****************End Prometheus Config Processing********************"
+            end
           end
         rescue => errorStr
-          # error handling code
+          puts "config::error::Exception while reading config file for prometheus config for replicaset: #{errorStr}, using defaults"
         end
-    elsif controller.casecmp(@daemonset) == 0 && !parsedConfig[:prometheus_data_collection_settings][:node].nil?
+      elsif controller.casecmp(@daemonset) == 0 && !parsedConfig[:prometheus_data_collection_settings][:node].nil?
         #Get prometheus daemonset custom config settings
         begin
           interval = parsedConfig[:prometheus_data_collection_settings][:node][:interval]
@@ -108,37 +102,32 @@ end
           fieldDrop = parsedConfig[:prometheus_data_collection_settings][:node][:fielddrop]
           urls = parsedConfig[:prometheus_data_collection_settings][:node][:urls]
 
-          if checkForType(interval, String) 
-            && checkForTypeArray(fieldPass, String)
-            && checkForTypeArray(fieldDrop, String)
-            && checkForTypeArray(urls, String)
-
+          if checkForType(interval, String) && checkForTypeArray(fieldPass, String) && checkForTypeArray(fieldDrop, String) && checkForTypeArray(urls, String)
             # Write the settings to file, so that they can be set as environment variables
-file = File.open("prom_config_env_var", "w")
+            file = File.open("prom_config_env_var", "w")
 
-if !file.nil?
-  file.write("export AZMON_DS_PROM_INTERVAL=#{interval}\n")
-  file.write("export AZMON_DS_PROM_FIELDPASS=\"#{fieldPass}\"\n")
-  file.write("export AZMON_DS_PROM_FIELDDROP=#{fieldDrop}\n")
-  file.write("export AZMON_DS_PROM_URLS=#{urls}\n")
-  # Close file after writing all environment variables
-  file.close
-  puts "****************End Prometheus Config Processing********************"
-else
-  puts "config::error::Exception while opening file for writing prometheus daemonset config environment variables"
-  puts "****************End Prometheus Config Processing********************"
-end
-
+            if !file.nil?
+              file.write("export AZMON_DS_PROM_INTERVAL=#{interval}\n")
+              file.write("export AZMON_DS_PROM_FIELDPASS=\"#{fieldPass}\"\n")
+              file.write("export AZMON_DS_PROM_FIELDDROP=#{fieldDrop}\n")
+              file.write("export AZMON_DS_PROM_URLS=#{urls}\n")
+              # Close file after writing all environment variables
+              file.close
+              puts "****************End Prometheus Config Processing********************"
+            else
+              puts "config::error::Exception while opening file for writing prometheus daemonset config environment variables"
+              puts "****************End Prometheus Config Processing********************"
+            end
           end
         rescue => errorStr
-          # error handling code
+          puts "config::error::Exception while reading config file for prometheus config for daemonset: #{errorStr}, using defaults"
         end
-          end
-        end
-      end
-
+      end # end for controller type check
     end
+  else
+    puts "config::error:: Controller undefined, using defaults"
   end
+end
 
 @promConfigSchemaVersion = ENV["AZMON_AGENT_PROM_CFG_SCHEMA_VERSION"]
 puts "****************Start Prometheus Config Processing********************"
@@ -152,35 +141,4 @@ else
     puts "config::unsupported/missing config schema version for prometheus config - '#{@promConfigSchemaVersion}' , using defaults"
   end
   # @excludePath = "*_kube-system_*.log"
-end
-
-# Write the settings to file, so that they can be set as environment variables
-file = File.open("prom_config_env_var", "w")
-
-if !file.nil?
-  # This will be used in td-agent-bit.conf file to filter out logs
-  if (!@collectStdoutLogs && !@collectStderrLogs)
-    #Stop log tailing completely
-    @logTailPath = "/opt/nolog*.log"
-    @logExclusionRegexPattern = "stdout|stderr"
-  elsif !@collectStdoutLogs
-    @logExclusionRegexPattern = "stdout"
-  elsif !@collectStderrLogs
-    @logExclusionRegexPattern = "stderr"
-  end
-  file.write("export AZMON_COLLECT_STDOUT_LOGS=#{@collectStdoutLogs}\n")
-  file.write("export AZMON_LOG_TAIL_PATH=#{@logTailPath}\n")
-  file.write("export AZMON_LOG_EXCLUSION_REGEX_PATTERN=\"#{@logExclusionRegexPattern}\"\n")
-  file.write("export AZMON_STDOUT_EXCLUDED_NAMESPACES=#{@stdoutExcludeNamespaces}\n")
-  file.write("export AZMON_COLLECT_STDERR_LOGS=#{@collectStderrLogs}\n")
-  file.write("export AZMON_STDERR_EXCLUDED_NAMESPACES=#{@stderrExcludeNamespaces}\n")
-  file.write("export AZMON_CLUSTER_COLLECT_ENV_VAR=#{@collectClusterEnvVariables}\n")
-  file.write("export AZMON_CLUSTER_LOG_TAIL_EXCLUDE_PATH=#{@excludePath}\n")
-  # Close file after writing all environment variables
-  file.close
-  puts "Both stdout & stderr log collection are turned off for namespaces: '#{@excludePath}' "
-  puts "****************End Config Processing********************"
-else
-  puts "config::error::Exception while opening file for writing prometheus config environment variables"
-  puts "****************End Prometheus Config Processing********************"
 end
