bfbbec3c168df1643f7bfe1ab3de639ea7436bee
rashmy <rashmy@RASHMY-PC2>
2019-08-02 15:25:50 -0700

    changes

---
+++ installer/scripts/td-agent-bit-conf-customizer.rb
@@ -0,0 +1,46 @@
+#!/usr/local/bin/ruby
+
+@td_agent_bit_conf_path = "/etc/opt/microsoft/docker-cimprov/td-agent-bit.conf"
+@default_service_interval = 15
+
+def is_number?(string)
+  true if Integer(string) rescue false
+end
+
+def substituteFluebtBitPlaceHolders
+  begin
+    # Replace the fluentbit config file with custom values if present
+    puts "config::Starting to substitute the placeholders in td-agent-bit.conf file for log collection"
+
+    interval = ENV["FBIT_SERVICE_FLUSH_INTERVAL"]
+    bufferChunkSize = ENV["FBIT_TAIL_BUFFER_CHUNK_SIZE"]
+    bufferMaxSize = ENV["FBIT_TAIL_BUFFER_MAX_SIZE"]
+
+    serviceInterval = (!interval.nil? && is_number?(interval)) ? interval : @default_service_interval
+    serviceIntervalSetting = "Flush         " + serviceInterval
+
+    tailBufferChunkSize = (!bufferChunkSize.nil? && is_number?(bufferChunkSize)) ? bufferChunkSize : nil
+    # tailBufferChunkSizeSetting = (!tailBufferChunkSize.nil?) ? ("Buffer_Chunk_Size " + tailBufferChunkSize + "m") : ""
+
+    tailBufferMaxSize = (!bufferMaxSize.nil? && is_number?(bufferMaxSize)) ? bufferMaxSize : nil
+    tailBufferMaxSizeSetting = (!tailBufferMaxSize.nil?) ? ("Buffer_Max_Size " + tailBufferMaxSize + "m") : ""
+
+    text = File.read(@td_agent_bit_conf_path)
+    new_contents = text.gsub("${SERVICE_FLUSH_INTERVAL}", serviceIntervalSetting)
+    if !tailBufferChunkSize.nil?
+      new_contents = new_contents.gsub("${TAIL_BUFFER_CHUNK_SIZE}", "Buffer_Chunk_Size " + tailBufferChunkSize + "m")
+    else
+      new_contents = new_contents.gsub("\n    ${TAIL_BUFFER_CHUNK_SIZE}\n", "\n")
+    end
+    if !tailBufferMaxSize.nil?
+      new_contents = new_contents.gsub("${TAIL_BUFFER_MAX_SIZE}", "Buffer_Max_Size " + tailBufferMaxSize + "m")
+    else
+      new_contents = new_contents.gsub("\n    ${TAIL_BUFFER_MAX_SIZE}\n", "\n")
+    end
+
+    File.open(file_name, "w") { |file| file.puts new_contents }
+    puts "config::Successfully substituted the placeholders in td-agent-bit.conf file"
+  rescue => errorStr
+    puts "td-agent-bit-config-customizer: error while substituting values: #{errorStr}"
+  end
+end
