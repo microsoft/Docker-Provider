eb064b477daa9b783073c1be32ea73448a5386df
rashmy <rashmy@RASHMY-PC2>
2019-09-17 19:45:38 -0700

    changes

--- source/code/go/src/plugins/oms.go
+++ source/code/go/src/plugins/oms.go
@@ -311,6 +311,10 @@ func populateErrorHash(record map[interface{}]interface{}, errType ErrorType) {
 	switch errType {
 	case ConfigError:
 		// Log("configErrorHash\n")
+		// Doing this since the error logger library is adding quotes around the string and a newline to the end because
+		// we are converting string to json to log lines in different lines as one record
+		logRecordString = logRecordString[1 : len(logRecordString)-1]
+		logRecordString = strings.TrimSuffix(logRecordString, "\n")
 		ConfigErrorHash[logRecordString] = ConfigErrorDetails{
 			ContainerId:    containerID,
 			PodName:        podName,
