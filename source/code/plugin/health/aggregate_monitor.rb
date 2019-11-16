311fdbda66e43377d520faff820c38295cfa4422
rashmy <rashmy@RASHMY-PC2>
2019-11-15 17:42:18 -0800

    changing to have morgan changes only

--- source/code/plugin/health/aggregate_monitor.rb
+++ source/code/plugin/health/aggregate_monitor.rb
@@ -3,12 +3,6 @@
 require_relative 'health_model_constants'
 require 'yajl/json_gem'
 
-# Require only when running inside container.
-# otherwise unit tests will fail due to ApplicationInsightsUtility dependency on base omsagent ruby files. If you have your dev machine starting with omsagent-rs, then GOOD LUCK!
-if Socket.gethostname.start_with?('omsagent-rs')
-    require_relative '../ApplicationInsightsUtility'
-end
-
 module HealthModel
   class AggregateMonitor
     attr_accessor :monitor_id, :monitor_instance_id, :state, :transition_date_time, :aggregation_algorithm, :aggregation_algorithm_params, :labels, :is_aggregate_monitor, :details
@@ -21,8 +15,6 @@ module HealthModel
         MonitorState::HEALTHY => 4,
         MonitorState::NONE => 5
     }
-
-    @@telemetry_sent_hash = {}
 
     # constructor
     def initialize(
@@ -135,43 +127,17 @@ module HealthModel
 
         #sort
         #TODO: What if sorted_filtered is empty? is that even possible?
-        log = HealthMonitorHelpers.get_log_handle
         sorted_filtered = sort_filter_member_monitors(monitor_set)
 
         state_threshold = @aggregation_algorithm_params['state_threshold'].to_f
 
-        if sorted_filtered.nil?
-            size = 0
-        else
-            size = sorted_filtered.size
-        end
-
+        size = sorted_filtered.size
         if size == 1
             @state =  sorted_filtered[0].state
         else
             count = ((state_threshold*size)/100).ceil
             index = size - count
-            if sorted_filtered.nil? || sorted_filtered[index].nil?
-                @state = HealthMonitorStates::UNKNOWN
-                if !@@telemetry_sent_hash.key?(@monitor_instance_id)
-                    log.debug "Adding to telemetry sent hash #{@monitor_instance_id}"
-                    @@telemetry_sent_hash[@monitor_instance_id] = true
-                    log.info "Index: #{index} size: #{size} Count: #{count}"
-                    custom_error_event_map = {}
-                    custom_error_event_map["count"] = count
-                    custom_error_event_map["index"] = index
-                    custom_error_event_map["size"] = size
-                    if !sorted_filtered.nil?
-                        sorted_filtered.each_index{|i|
-                            custom_error_event_map[i] = sorted_filtered[i].state
-                        }
-                    end
-                    ApplicationInsightsUtility.sendCustomEvent("PercentageStateCalculationErrorEvent", custom_error_event_map)
-                end
-            else
-                @state = sorted_filtered[index].state
-            end
-            @state
+            @state = sorted_filtered[index].state
         end
     end
 
