a72f0a55e7c0aec16569fe8b87ca42e068e18fc4
Rashmi Mysore Chandrashekar <rashmy@microsoft.com>
2021-03-15 18:40:09 -0700

    getting telegraf build from pre release

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -61,7 +61,13 @@ sudo apt-get install libcap2-bin -y
 #service telegraf stop
 
 #wget https://github.com/microsoft/Docker-Provider/releases/download/5.0.0.0/telegraf
-wget https://github.com/microsoft/Docker-Provider/releases/download/telegraf-test-pr/telegraf
+#wget https://github.com/microsoft/Docker-Provider/releases/download/telegraf-test-pr/telegraf
+
+#1.18 pre-release
+wget https://dl.influxdata.com/telegraf/releases/telegraf-1.18.0~rc1_linux_amd64.tar.gz
+tar -zxvf telegraf-1.18.0~rc1_linux_amd64.tar.gz
+
+mv /opt/telegraf-1.18.0/usr/bin/telegraf /opt/telegraf
 
 chmod 777 /opt/telegraf
 
