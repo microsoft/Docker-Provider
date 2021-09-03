089abebe377a5818dfd7e6fc7b385c3de56cfed0
sarahpeiffer <speiffer@wustl.edu>
2021-09-03 16:52:17 -0700

    removing charts from tarball

--- .pipelines/build-linux.sh
+++ .pipelines/build-linux.sh
@@ -17,10 +17,11 @@ cd $DIR
 
 echo "------------ Bundle Shell Extension Scripts for Agent Release -------------------------"
 cd $DIR/../deployment/agent-deployment/ServiceGroupRoot/Scripts
-tar -czvf ../artifacts.tar.gz ../../../../charts/azuremonitor-containers/ pushAgentToAcr.sh
+tar -czvf ../artifacts.tar.gz pushAgentToAcr.sh
 cd $DIR
 
 echo "------------ Bundle Shell Extension Scripts & HELM chart -------------------------"
 cd $DIR/../deployment/arc-k8s-extension/ServiceGroupRoot/Scripts
 tar -czvf ../artifacts.tar.gz ../../../../charts/azuremonitor-containers/ pushChartToAcr.sh
+
 
