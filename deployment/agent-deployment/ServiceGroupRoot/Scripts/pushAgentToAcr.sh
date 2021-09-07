532259450c8cf7361ef8b843780e77410b573b9a
sarahpeiffer <speiffer@wustl.edu>
2021-09-07 14:59:14 -0700

    change true check

--- deployment/agent-deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
+++ deployment/agent-deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
@@ -95,7 +95,7 @@ BLOB_EXIST=$(echo "$BLOB_EXIST_RESULT" | jq -r '.exists')
 echo $BLOB_EXIST_RESULT
 echo $BLOB_EXIST
 
-if [[ $BLOB_EXIST ]]; then
+if $BLOB_EXIST; then
   az storage blob delete --container-name "agent" --name "${RELEASE_ID}" --account-name "cipipelinestorageev2" --sas-token "${AGENT_IMAGE_SAS}"
   echo "Deleted agentimate copy from blob storage"
 else 
