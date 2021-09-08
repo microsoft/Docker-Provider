05e9921360a1b49462a40959880d9976078e8782
sarahpeiffer <speiffer@wustl.edu>
2021-09-07 17:01:49 -0700

    comments and change storage account info to pipeline variables

--- deployment/agent-deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
+++ deployment/agent-deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
@@ -3,28 +3,6 @@ set -e
 
 # Note - This script used in the pipeline as inline script
 
-# These are plain pipeline variable which can be modified anyone in the team
-# AGENT_RELEASE=ciprod
-# AGENT_IMAGE_TAG_SUFFIX=08202021
-
-# LINUX_AGENT_IMAGE_FULL_PATH=${ACR_NAME}/public/azuremonitor/containerinsights/${AGENT_RELEASE}:${AGENT_RELEASE}${AGENT_IMAGE_TAG_SUFFIX}
-# WINDOWS_AGENT_IMAGE_FULL_PATH=${ACR_NAME}/public/azuremonitor/containerinsights/${AGENT_RELEASE}:win-${AGENT_RELEASE}${AGENT_IMAGE_TAG_SUFFIX}
-
-# if [ -z ${AGENT_IMAGE_TAR_FILE_NAME+x} ]; then
-#     echo "AGENT_IMAGE_TAR_FILE_NAME is unset, unable to continue"
-#     exit 1;
-# fi
-
-# CWD=$PWD
-# echo "current working directory"
-# echo $CWD
-
-# cd ..
-# ls
-# cd Scripts
-
-
-
 if [ -z $AGENT_IMAGE_TAG_SUFFIX ]; then
   echo "-e error value of AGENT_IMAGE_TAG_SUFFIX variable shouldnt be empty"
   exit 1
@@ -40,19 +18,10 @@ if [ -z $AGENT_RELEASE ]; then
   exit 1
 fi
 
-echo $AGENT_IMAGE_TAG_SUFFIX
-
-echo $AGENT_IMAGE_URI
-
-echo $AGENT_IMAGE_SAS
-
-echo $RELEASE_ID
-
-
-echo "Downloading docker tarball image from $TARBALL_IMAGE_FILE_SAS"
+#Download agentimage tarball from blob storage account
+echo "Downloading tarball image from $AGENT_IMAGE_URI"
 wget -O $AGENT_IMAGE_TAR_FILE_NAME "${AGENT_IMAGE_URI}${RELEASE_ID}${AGENT_IMAGE_SAS}"
 
-ls
 
 if [ ! -f $AGENT_IMAGE_TAR_FILE_NAME ]; then
     echo "Agent tarfile: ${AGENT_IMAGE_TAR_FILE_NAME} does not exist, unable to continue"
@@ -65,9 +34,10 @@ wget -O crane.tar.gz https://github.com/google/go-containerregistry/releases/dow
 tar xzvf crane.tar.gz
 echo "Installed crane"
 
-#echo "Login cli using managed identity"
-az login --identity
 
+#Login to az cli and authenticate to acr
+echo "Login cli using managed identity"
+az login --identity
 
 echo "Getting acr credentials"
 TOKEN_QUERY_RES=$(az acr login -n "$ACR_NAME" -t)
@@ -75,6 +45,7 @@ TOKEN=$(echo "$TOKEN_QUERY_RES" | jq -r '.accessToken')
 DESTINATION_ACR=$(echo "$TOKEN_QUERY_RES" | jq -r '.loginServer')
 ./crane auth login "$DESTINATION_ACR" -u "00000000-0000-0000-0000-000000000000" -p "$TOKEN"
 
+#Prepare tarball and push to acr
 if [[ "$AGENT_IMAGE_TAR_FILE_NAME" == *"tar.gz"* ]]; then
   gunzip $AGENT_IMAGE_TAR_FILE_NAME
 fi
@@ -86,17 +57,17 @@ fi
 echo "Pushing file $TARBALL_IMAGE_FILE to $AGENT_IMAGE_FULL_PATH"
 ./crane push *.tar "$AGENT_IMAGE_FULL_PATH"
 
+
+#Delete agentimage tarball from blob storage to prevent future conflicts
 echo "Deleting agentimage copy from blob storage"
 
-echo $RELEASE_ID
-
-BLOB_EXIST_RESULT=$(az storage blob exists --container-name agent --name $RELEASE_ID --account-name cipipelinestorageev2 --sas-token $AGENT_IMAGE_SAS)
+BLOB_EXIST_RESULT=$(az storage blob exists --container-name $STORAGE_CONTAINER_NAME --name $RELEASE_ID --account-name $STORAGE_ACCOUNT_NAME --sas-token $AGENT_IMAGE_SAS)
 BLOB_EXIST=$(echo "$BLOB_EXIST_RESULT" | jq -r '.exists')
 echo $BLOB_EXIST_RESULT
 echo $BLOB_EXIST
 
 if $BLOB_EXIST; then
-  az storage blob delete --container-name "agent" --name "${RELEASE_ID}" --account-name "cipipelinestorageev2" --sas-token "${AGENT_IMAGE_SAS}"
+  az storage blob delete --container-name "${STORAGE_CONTAINER_NAME}" --name "${RELEASE_ID}" --account-name "${STORAGE_ACCOUNT_NAME}" --sas-token "${AGENT_IMAGE_SAS}"
   echo "Deleted agentimate copy from blob storage"
 else 
     echo "Agentimage has already been deleted from blob storage"
