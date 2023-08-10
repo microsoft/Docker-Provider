7743f41d4ffb22fb951aded601cdbec7c1bdc520
Alexander Kaplan <alkaplan@microsoft.com>
2023-08-09 19:57:12 -0700

    Minor tweaks

--- appmonitoring/ts/src/dockerBuild.cmd
+++ appmonitoring/ts/src/dockerBuild.cmd
@@ -6,4 +6,6 @@ call az acr login -n aicommon || echo ACR login failed && exit /b
 
 call docker buildx build --platform linux/amd64 --tag aicommon.azurecr.io/aidev:%1 -f ./Dockerfile --push --provenance=false . || echo Docker build failed && exit /b
 
+echo Done
+
 endlocal
