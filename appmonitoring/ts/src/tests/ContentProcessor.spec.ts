5b423c8694f9802cf6cd51f0170f2ba5f29e05e7
Alexander Kaplan <alkaplan@microsoft.com>
2023-08-09 19:33:35 -0700

    Fixing unit test

--- appmonitoring/ts/src/tests/ContentProcessor.spec.ts
+++ appmonitoring/ts/src/tests/ContentProcessor.spec.ts
@@ -122,7 +122,6 @@ describe("ContentProcessor", () => {
     })
 
     it("DiffCalculatorNull1", async () => {
-        expect(1).toBe(0);
         expect(await DiffCalculator.CalculateDiff(null, null, null, null, null, null, null)).toBeNull();
     })
 
