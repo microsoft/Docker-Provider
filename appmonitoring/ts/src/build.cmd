7ac07c53794bcd87abde03cba02a03ff13fb8f7a
Alexander Kaplan <alkaplan@microsoft.com>
2023-08-09 19:52:57 -0700

    Build script improvements

--- appmonitoring/ts/src/build.cmd
+++ appmonitoring/ts/src/build.cmd
@@ -1,4 +1,6 @@
-call del *.js.map
-call del *.js
+del *.js.map
+del *.js
 
-call tsc --build && npx eslint . && npm test
+call tsc --build || echo Build failed && exit /b
+call npx eslint . || echo ESLint failed && exit /b 
+call npm test || echo Jest failed && exit /b
