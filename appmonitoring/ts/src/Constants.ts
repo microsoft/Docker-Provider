a3c854476bd14a2ff49c8b71129c10c9b03b6a9e
ramanujbhattacharjee93 <rambhatt@microsoft.com>
2023-08-14 15:58:54 -0700

    add comment

--- appmonitoring/ts/src/Constants.ts
+++ appmonitoring/ts/src/Constants.ts
@@ -1,4 +1,4 @@
-export const WebhookDNSEndpoint = 'app-monitoring-webhook-service.kube-system.svc';
+export const WebhookDNSEndpoint = 'app-monitoring-webhook-service.kube-system.svc'; //This is also defined in _omsagent.yaml in AKS RP and must always match the value there
 export const CertificateStoreName = 'app-monitoring-webhook-cert';
 export const WebhookName = 'app-monitoring-webhook';
 export const NamespaceName = 'kube-system';
