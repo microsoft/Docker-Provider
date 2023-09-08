be5fdbb3ab40445441a694d1783e81945d4eecdb
Cameron Carlton (from Dev Box) <cacarlt@microsoft.com>
2023-09-08 11:46:38 -0700

    Updating deploy-infra script

---
+++ test/whl-scale-tests/common.ps1
@@ -0,0 +1,39 @@
+function Get-RandomPassword {
+    param (
+        [Parameter(Mandatory)]
+        [int] $length
+    )
+    $charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789{-)}$^%(_!#'.ToCharArray()
+    $rng = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
+    $bytes = New-Object byte[]($length)
+ 
+    $rng.GetBytes($bytes)
+ 
+    $result = New-Object char[]($length)
+ 
+    for ($i = 0 ; $i -lt $length ; $i++) {
+        $result[$i] = $charSet[$bytes[$i]%$charSet.Length]
+    }
+ 
+    return (-join $result).ToString()
+}
+
+
+function SubstituteNameValuePairs {
+    param(
+        [string][Parameter(Mandatory=$true)] $InputFilePath,
+        [string][Parameter(Mandatory=$true)] $OutputFilePath,
+        [hashtable][Parameter(Mandatory=$true)] $Substitutions
+    )
+
+    # Ensure the input file exists
+    if (-not (Test-Path -Path $InputFilePath)) {
+        Write-Host "  Input File: '$InputFilePath' does not exist" -ForegroundColor Red
+        exit
+    }
+
+    foreach($subItem in $Substitutions.GetEnumerator())
+    {
+        (Get-Content $InputFilePath).Replace($subItem.Name, $subItem.Value) | Set-Content $OutputFilePath
+    }
+}
--- test/whl-scale-tests/deploy-infra.ps1
+++ test/whl-scale-tests/deploy-infra.ps1
@@ -1,41 +1,25 @@
 param(
     [guid] [Parameter(Mandatory = $true)] $SubscriptionId,
     [string] [Parameter(Mandatory = $true)] $Location,
-    [string] [Parameter(Mandatory = $false)] $ResourceGroupName = [Environment]::UserName + "scaletest",
-    [string] [Parameter(Mandatory = $false)] $AKSClusterName = $ResourceGroupName + "aks",
     [string] [Parameter(Mandatory = $false)]
              [ValidateSet("node-image", "none", "patch", "rapid", "stable")] $AKSAutoUpgradeChannel = "stable",
-    [int]    [Parameter(Mandatory = $false)] $AKSVersion = 1.26.3,
-    [int]    [Parameter(Mandatory = $false)] $AKSWindowsNodeCount = 5,
-    [string] [Parameter(Mandatory = $false)] $KeyVaultName = $ResourceGroupName + "-kv"
+    [string]    [Parameter(Mandatory = $false)] $AKSVersion = "1.26.3",
+    [int]    [Parameter(Mandatory = $false)] $AKSWindowsNodeCount = 1
 )
 
-function Get-RandomPassword {
-    param (
-        [Parameter(Mandatory)]
-        [int] $length
-    )
-    $charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789{]+-[*=@:)}$^%;(_!&amp;#?>/|.'.ToCharArray()
-    $rng = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
-    $bytes = New-Object byte[]($length)
- 
-    $rng.GetBytes($bytes)
- 
-    $result = New-Object char[]($length)
- 
-    for ($i = 0 ; $i -lt $length ; $i++) {
-        $result[$i] = $charSet[$bytes[$i]%$charSet.Length]
-    }
- 
-    return (-join $result).ToString()
-}
+. .\common.ps1
+
+$resourceGroupName = [Environment]::UserName + "scaletest"
+$acrName = $resourceGroupName + "acr"
+$aksClusterName = $resourceGroupName + "aks"
+$keyVaultName = $resourceGroupName + "kv"
 
 # Login using your microsoft accout
 Write-Host "Login with your Microsoft account"
 az login
 
 # Set subscription
-Write-Host "Set az account to given Subscription"
+Write-Host "Setting Azure CLI to given Subscription       "
 az account set --subscription $SubscriptionId
 
 # Required for Windows node pool and could be used to troubleshoot any issues
@@ -43,16 +27,22 @@ Write-Host "Creating random password for Host login"
 $password = Get-RandomPassword 16
 
 # Create resource group
-Write-Host "Creating resource group"
-az group create --name $ResourceGroupName --location $Location
+Write-Host "Creating Azure Resource Group"
+az group create --name $resourceGroupName --location $Location
+
+# Create Azure Container Registery
+Write-Host "Creating Azure Container Registery"
+az acr create --resource-group $resourceGroupName --name $acrName --sku Standard
 
 # Create an AKS cluster with a Linux node pool
-Write-Host "Creating AKS Cluster with Linux node pool"
+#$ACRUri = $acrName + ".azurecr.io"
+Write-Host "Creating AKS Cluster with System node pool"
 az aks create `
-    --resource-group $ResourceGroupName `
-    --name $AKSClusterName `
+    --resource-group $resourceGroupName `
+    --name $aksClusterName `
     --network-plugin azure `
     --node-vm-size Standard_D2s_v3 `
+    --attach-acr $acrName `
     --auto-upgrade-channel $AKSAutoUpgradeChannel `
     --kubernetes-version $AKSVersion `
     --enable-managed-identity `
@@ -65,8 +55,9 @@ az aks create `
 # Create a Windows node pool for Text Log scale test
 Write-Host "Creating Windows Node Pool for Text Log scale test"
 az aks nodepool add `
-    --resource-group $ResourceGroupName `
-    --cluster-name $AKSClusterName `
+    --resource-group $resourceGroupName `
+    --cluster-name $aksClusterName `
+    --os-type Windows `
     --os-sku Windows2022 `
     --name  txtlog `
     --node-vm-size Standard_D2s_v3 `
@@ -75,8 +66,9 @@ az aks nodepool add `
 # Create a Windows node pool for ETW Log scale test"
 Write-Host "Creating Windows Node Pool for ETW Log scale test"
 az aks nodepool add `
-    --resource-group $ResourceGroupName `
-    --cluster-name $AKSClusterName `
+    --resource-group $resourceGroupName `
+    --cluster-name $aksClusterName `
+    --os-type Windows `
     --os-sku Windows2022 `
     --name  etwlog `
     --node-vm-size Standard_D2s_v3 `
@@ -85,8 +77,9 @@ az aks nodepool add `
 # Create a Windows node pool for Event Log scale test
 Write-Host "Creating Windows Node Pool for Event Log scale test"
 az aks nodepool add `
-    --resource-group $ResourceGroupName `
-    --cluster-name $AKSClusterName `
+    --resource-group $resourceGroupName `
+    --cluster-name $aksClusterName `
+    --os-type Windows `
     --os-sku Windows2022 `
     --name  evtlog `
     --node-vm-size Standard_D2s_v3 `
@@ -95,8 +88,9 @@ az aks nodepool add `
 # Create a Windows node pool for Crash Dump scale test
 Write-Host "Creating Windows Node Pool for Crash Dump scale test"
 az aks nodepool add `
-    --resource-group $ResourceGroupName `
-    --cluster-name $AKSClusterName `
+    --resource-group $resourceGroupName `
+    --cluster-name $aksClusterName `
+    --os-type Windows `
     --os-sku Windows2022 `
     --name  crashd `
     --node-vm-size Standard_D2s_v3 `
@@ -104,10 +98,10 @@ az aks nodepool add `
 
 # Create a Key Vault
 Write-Host "Creating a key vault"
-az keyvault create --name $KeyVaultName --resource-group $ResourceGroupName --location $Location
+az keyvault create --name $keyVaultName --resource-group $resourceGroupName --location $Location
 
 # Add password to as secret in the key vault
 Write-Host "Adding Windows Node Pool passwrod to Key Vault"
-az keyvault secret set --vault-name $KeyVaultName --name "WindowsScaleTest" --value $password
+az keyvault secret set --vault-name $keyVaultName --name "WindowsScaleTest" --value $password
 
 Write-Host "Windows Host Log Scale Test Infrastructure deployed"
