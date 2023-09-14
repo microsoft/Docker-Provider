d4d6c777ef0d9ac5642df616b3c3b2f85f1e0fbb
Janvi Jatakia (from Dev Box) <jajataki@microsoft.com>
2023-09-14 14:38:14 -0700

    Update utils_linux.go

--- source/plugins/go/src/utils_linux.go
+++ source/plugins/go/src/utils_linux.go
@@ -2,7 +2,15 @@
 
 package main
 
-func CreateWindowsNamedPipesClient(namedPipe string) {
+import "net"
+
+func CreateWindowsNamedPipeClient(namedPipe string, namedPipeConnection *net.Conn) {
 	//function unimplemented
-	Log("Error::CreateWindowsNamedPipesClient not implemented for Linux")
+	Log("Error::CreateWindowsNamedPipeClient not implemented for Linux")
+}
+
+func CreateGenevaOr3PNamedPipe(namedPipeConnection *net.Conn, datatype string, errorCount *float64, isGenevaLogsIntegrationEnabled bool) bool {
+	//function unimplemented
+	Log("Error::CreateGenevaOr3PNamedPipe not implemented for Linux")
+	return false
 }
