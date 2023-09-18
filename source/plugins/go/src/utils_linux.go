f79e0a90c56791d81fe5a94525f2a121bbe38235
Janvi Jatakia (from Dev Box) <jajataki@microsoft.com>
2023-09-18 16:48:08 -0700

    Updated linux unimplemented function

--- source/plugins/go/src/utils_linux.go
+++ source/plugins/go/src/utils_linux.go
@@ -9,7 +9,7 @@ func CreateWindowsNamedPipeClient(namedPipe string, namedPipeConnection *net.Con
 	Log("Error::CreateWindowsNamedPipeClient not implemented for Linux")
 }
 
-func CreateGenevaOr3PNamedPipe(namedPipeConnection *net.Conn, datatype string, errorCount *float64, isGenevaLogsIntegrationEnabled bool) bool {
+func CreateGenevaOr3PNamedPipe(namedPipeConnection *net.Conn, datatype string, errorCount *float64, isGenevaLogsIntegrationEnabled bool, refreshTracker *time.Time) bool {
 	//function unimplemented
 	Log("Error::CreateGenevaOr3PNamedPipe not implemented for Linux")
 	return false
