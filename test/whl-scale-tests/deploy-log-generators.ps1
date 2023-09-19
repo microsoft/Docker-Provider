6f0babf5c8b3dbb7e33969f5465893d88e52d1c9
Chris Raitano <craitano@microsoft.com>
2023-09-18 17:23:08 -0700

    Text Logs Scale Testing

--- test/whl-scale-tests/deploy-log-generators.ps1
+++ test/whl-scale-tests/deploy-log-generators.ps1
@@ -96,6 +96,6 @@ if($EventLogs -or $all){
 
 if($TextLogs -or $all){
   Write-Host "START:Deploying Text Log Generator"
-  # TODO: Deploy text log generator here
+  buildAndDeploy "$acrUri/generatetextlogs:latest" "whl-text-log-generator" "whl-txtlog" "txtlog" "$PSScriptRoot\text-logs"
   Write-Host "END:Deploying Text Log Generator"
 }
