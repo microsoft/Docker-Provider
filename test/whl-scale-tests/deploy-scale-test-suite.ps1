a8efec7e9cb6b389a586b1ce9cec58b86d25a1d5
cacarlt <cacarlt@microsoft.com>
2023-10-02 13:54:34 -0700

    Fixed sleep issues

--- test/whl-scale-tests/deploy-scale-test-suite.ps1
+++ test/whl-scale-tests/deploy-scale-test-suite.ps1
@@ -30,7 +30,7 @@ az account set --subscription $SubscriptionId
 #Use Windows Engine on Docker
 Write-Host "Setting Docker to utilize Windows Engine"
 Start-Process -filePath "Docker Desktop.exe" -WorkingDirectory "C:\Program Files\Docker\Docker"
-Start-Sleep -Duration (New-TimeSpan -Seconds 60)
+Start-Sleep -Seconds 60
 Start-Process -filePath "DockerCli.exe" -WorkingDirectory "C:\Program Files\Docker\Docker" -ArgumentList "-SwitchWindowsEngine"
 
 #Login into ACR
@@ -104,11 +104,11 @@ kubectl apply -f .\host-logs-geneva.yaml
 
 Write-Host "Waiting for pod to be ready..."
 
-Start-Sleep -Duration (New-TimeSpan -Seconds 180)
+Start-Sleep -Seconds 180
 kubectl get pods -n $whlCrashDumpNamespace
 
 kubectl apply -f .\container-azm-ms-agentconfig.yaml
-Start-Sleep -Duration (New-TimeSpan -Seconds 180)
+Start-Sleep -Seconds 180
 kubectl get pods -n $whlCrashDumpNamespace
 
 #Targeting WHL for Event Log Configuration
@@ -137,11 +137,11 @@ kubectl apply -f .\host-logs-geneva.yaml
 
 Write-Host "Waiting..."
 
-Start-Sleep -Duration (New-TimeSpan -Seconds 180)
+Start-Sleep -Seconds 180
 kubectl get pods -n $whlEventLogNamespace
 
 kubectl apply -f .\container-azm-ms-agentconfig.yaml
-Start-Sleep -Duration (New-TimeSpan -Seconds 180)
+Start-Sleep -Seconds 180
 kubectl get pods -n $whlEventLogNamespace
 
 #Targeting WHL for ETW Log Configuration
@@ -170,11 +170,11 @@ kubectl apply -f .\host-logs-geneva.yaml
 
 Write-Host "Waiting..."
 
-Start-Sleep -Duration (New-TimeSpan -Seconds 180)
+Start-Sleep -Seconds 180
 kubectl get pods -n $whlETWLogNamespace
 
 kubectl apply -f .\container-azm-ms-agentconfig.yaml
-Start-Sleep -Duration (New-TimeSpan -Seconds 180)
+Start-Sleep -Seconds 180
 kubectl get pods -n $whlETWLogNamespace
 
 #Targeting WHL for Text Log Configuration
@@ -203,11 +203,11 @@ kubectl apply -f .\host-logs-geneva.yaml
 
 Write-Host "Waiting..."
 
-Start-Sleep -Duration (New-TimeSpan -Seconds 180)
+Start-Sleep -Seconds 180
 kubectl get pods -n $whlTextLogNamespace
 
 kubectl apply -f .\container-azm-ms-agentconfig.yaml
-Start-Sleep -Duration (New-TimeSpan -Seconds 180)
+Start-Sleep -Seconds 180
 kubectl get pods -n $whlTextLogNamespace
 
 Set-Location -Path $orignalPath.path
