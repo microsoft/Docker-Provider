a9a93a176b95cc2c8a238399798bcf5b4f2bf59f
Sunil Yadav <suyadav@microsoft.com>
2025-01-24 12:15:56 -0800

    Tests to check running process

--- test/ginkgo-e2e/containerstatus/containerstatus_spec_test.go
+++ test/ginkgo-e2e/containerstatus/containerstatus_spec_test.go
@@ -33,7 +33,7 @@ var _ = DescribeTable("The pods should be scheduled in all nodes",
 		Expect(err).NotTo(HaveOccurred())
 	},
 	Entry("when checking the ama-logs", "kube-system", "component", "ama-logs-agent", "linux"),
-	Entry("when checking the ama-logs-win pod", "kube-system", "component", "ama-logs-agent", "windows", Label(utils.WindowsLabel)),
+	Entry("when checking the ama-logs-win pod", "kube-system", "component", "ama-logs-agent-windows", "windows", Label(utils.WindowsLabel)),
 )
 
 /*
@@ -46,7 +46,7 @@ var _ = DescribeTable("The pods should be scheduled in all Fips and ARM64 nodes"
 		Expect(err).NotTo(HaveOccurred())
 	},
 	Entry("when checking the ama-logs", "kube-system", "component", "ama-logs-agent", "kubernetes.azure.com/fips_enabled", "true", Label(utils.FIPSLabel)),
-	Entry("when checking the ama-logs-win pod", "kube-system", "component", "ama-logs-agent", "kubernetes.azure.com/fips_enabled", "true", Label(utils.WindowsLabel), Label(utils.FIPSLabel)),
+	Entry("when checking the ama-logs-win pod", "kube-system", "component", "ama-logs-agent-windows", "kubernetes.azure.com/fips_enabled", "true", Label(utils.WindowsLabel), Label(utils.FIPSLabel)),
 	Entry("when checking the ama-logs", "kube-system", "component", "ama-logs-agent", "kubernetes.io/arch", "arm64", Label(utils.ARM64Label)),
 )
 
@@ -59,56 +59,57 @@ var _ = DescribeTable("All processes are running",
 		err := utils.CheckAllProcessesRunning(K8sClient, Cfg, labelName, labelValue, namespace, containerName, processes)
 		Expect(err).NotTo(HaveOccurred())
 	},
-	Entry("when checking the ama-logs-rs replica pod(s)", "kube-system", "rsName", "ama-logs-rs", "ama-logs",
+	Entry("when checking the ama-logs-rs replica pod", "kube-system", "rsName", "ama-logs-rs", "ama-logs",
 		[]string{
 			"fluent-bit",
-			// "otelcollector",
-			// "mdsd -a -A -e",
-			// "MetricsExtension",
-			// "inotifywait /etc/config/settings",
+			"fluentd",
+			"mdsd -a -A -r", //this is -e for prometheus
+			"inotifywait /etc/config/settings",
 			// "inotifywait /etc/mdsd.d",
-			// "crond",
+			// ruby ??
+			// "telegraf",
+			"crond",
 		},
 	),
 	Entry("when checking the ama-logs daemonset pods", "kube-system", "component", "ama-logs-agent", "ama-logs",
 		[]string{
 			"fluent-bit",
-			// "otelcollector",
-			// "mdsd -a -A -e",
-			// "MetricsExtension",
-			// "inotifywait /etc/config/settings",
+			"fluentd",
+			"mdsd -a -A -r",
+			"inotifywait /etc/config/settings",
 			// "inotifywait /etc/mdsd.d",
-			// "crond",
+			// ruby ??
+			"crond",
+			"telegraf",
 		},
 	),
 )
 
 /*
-* For windows daemonset pods that have the ama-logs container, check all expected processes are running.
+* For windows daemonset pods that have the ama-logs-windows container, check all expected processes are running.
  */
-// var _ = DescribeTable("All processes are running",
-// 	func(namespace, labelName, labelValue, containerName string, processes []string) {
-// 		err := utils.CheckAllWindowsProcessesRunning(K8sClient, Cfg, labelName, labelValue, namespace, containerName, processes)
-// 		Expect(err).NotTo(HaveOccurred())
-// 	},
-// 	Entry("when checking the ama-metrics-win-node daemonset pods", "kube-system", "component", "ama-logs-agent", "ama-logs",
-// 		[]string{
-// 			"fluent-bit",
-// 			// "otelcollector",
-// 			// "MetricsExtension",
-// 			// "MonAgentLauncher",
-// 			// "MonAgentHost",
-// 			// "MonAgentManager",
-// 			// "MonAgentCore",
-// 		},
-// 		Label(utils.WindowsLabel),
-// 		FlakeAttempts(3),
-// 	),
-// )
+var _ = DescribeTable("All processes are running",
+	func(namespace, labelName, labelValue, containerName string, processes []string) {
+		err := utils.CheckAllWindowsProcessesRunning(K8sClient, Cfg, labelName, labelValue, namespace, containerName, processes)
+		Expect(err).NotTo(HaveOccurred())
+	},
+	Entry("when checking the ama-logs-windows daemonset pods", "kube-system", "component", "ama-logs-agent-windows", "ama-logs-windows",
+		[]string{
+			"fluent-bit",
+			"MonAgentLauncher",
+			"MonAgentHost",
+			"MonAgentManager",
+			"MonAgentCore",
+			"telegraf",
+		},
+		Label(utils.WindowsLabel),
+		FlakeAttempts(3),
+	),
+)
 
 /*
 - For each of the pods that we deploy in our chart, ensure each container within that pod doesn't have errors in the logs.
-- The replicaset, daemonset, and kube-state-metrics are always deployed.
+- The replicaset and daemonset are always deployed.
 - The operator-targets and node-exporter workloads are checked if the 'operator' or 'arc-extension' label is included in the test run.
 - The label and values are provided to get a list of pods only with that label.
 */
@@ -118,11 +119,8 @@ var _ = DescribeTable("The container logs should not contain errors",
 		Expect(err).NotTo(HaveOccurred())
 	},
 	Entry("when checking the ama-logs-rs pods", "kube-system", "rsName", "ama-logs-rs"),
-	Entry("when checking the ama-logs", "kube-system", "component", "ama-logs-agent"),
-	// Entry("when checking the ama-metrics replica pods", "kube-system", "rsName", "ama-metrics", Label(utils.ARM64Label)),
-	// Entry("when checking the ama-metrics-node", "kube-system", "dsName", "ama-metrics-node", Label(utils.ARM64Label)),
-	// Entry("when checking the ama-metrics-win-node", "kube-system", "dsName", "ama-metrics-win-node", Label(utils.WindowsLabel)),
-	// Entry("when checking the ama-metrics-ksm pod", "kube-system", "app.kubernetes.io/name", "ama-metrics-ksm"),
-	// Entry("when checking the ama-metrics-operator-targets pod", "kube-system", "rsName", "ama-metrics-operator-targets", Label(utils.OperatorLabel)),
-	// Entry("when checking the prometheus-node-exporter pod", "kube-system", "app", "prometheus-node-exporter", Label(utils.ArcExtensionLabel)),
+	Entry("when checking the ama-logs daemonset pods", "kube-system", "component", "ama-logs-agent"),
+	Entry("when checking the ama-logs-rs pods", "kube-system", "rsName", "ama-logs-rs", Label(utils.ARM64Label)),
+	Entry("when checking the ama-logs daemonset pods", "kube-system", "component", "ama-logs-agent", Label(utils.ARM64Label)),
+	Entry("when checking the ama-logs-windows daemonset pods", "kube-system", "component", "ama-logs-agent-windows", Label(utils.WindowsLabel)),
 )
