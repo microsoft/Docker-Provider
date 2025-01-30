e5049bccd47e1a70caf872b6769b0e77dc0aea57
Sunil Yadav <suyadav@microsoft.com>
2025-01-30 11:56:50 -0800

    added livenessprobe for windows

--- test/ginkgo-e2e/livenessprobe/livenessprobe_test.go
+++ test/ginkgo-e2e/livenessprobe/livenessprobe_test.go
@@ -38,14 +38,14 @@ var _ = Describe("When replicaset ama-logs-rs container liveness probe detects t
 		// add telegraf check
 	)
 
-	// Specify("the container-azm-ms-aks-k8scluster configmap has updated, the container should restart", func() {
-	// 	err := utils.GetAndUpdateConfigMap(K8sClient, "container-azm-ms-aks-k8scluster", "kube-system")
-	// 	Expect(err).NotTo(HaveOccurred())
-	// 	err = utils.WatchForPodRestart(K8sClient, "kube-system", "rsName", "ama-logs-rs", 180, "ama-logs",
-	// 		"inotifyoutput.txt has been updated - config changed",
-	// 	)
-	// 	Expect(err).NotTo(HaveOccurred())
-	// })
+	Specify("the container-azm-ms-aks-k8scluster configmap has updated, the container should restart", func() {
+		err := utils.GetAndUpdateConfigMap(K8sClient, "container-azm-ms-aks-k8scluster", "kube-system")
+		Expect(err).NotTo(HaveOccurred())
+		err = utils.WatchForPodRestart(K8sClient, "kube-system", "rsName", "ama-logs-rs", 180, "ama-logs",
+			"inotifyoutput.txt has been updated - config changed",
+		)
+		Expect(err).NotTo(HaveOccurred())
+	})
 
 	// check for certifical renewal - do we need this given we rely on MSI now?
 })
@@ -73,41 +73,41 @@ var _ = Describe("When the daemonset ama-logs container liveness probe detects t
 		// add telegraf check
 	)
 
-	// It("the container-azm-ms-aks-k8scluster configmap has updated, the container should restart", Label(utils.LinuxDaemonsetCustomConfig), func() {
-	// 	err := utils.GetAndUpdateConfigMap(K8sClient, "container-azm-ms-aks-k8scluster", "kube-system")
-	// 	Expect(err).NotTo(HaveOccurred())
-	// 	err = utils.WatchForPodRestart(K8sClient, "kube-system", "component", "ama-logs-agent", 180, "ama-logs",
-	// 		"inotifyoutput.txt has been updated - config changed",
-	// 	)
-	// 	Expect(err).NotTo(HaveOccurred())
-	// })
+	It("the container-azm-ms-aks-k8scluster configmap has updated, the container should restart", Label(utils.LinuxDaemonsetCustomConfig), func() {
+		err := utils.GetAndUpdateConfigMap(K8sClient, "container-azm-ms-aks-k8scluster", "kube-system")
+		Expect(err).NotTo(HaveOccurred())
+		err = utils.WatchForPodRestart(K8sClient, "kube-system", "component", "ama-logs-agent", 180, "ama-logs",
+			"inotifyoutput.txt has been updated - config changed",
+		)
+		Expect(err).NotTo(HaveOccurred())
+	})
 })
 
-// var _ = Describe("When the windows ama-logs-windows container liveness probe detects that", Ordered, Label(utils.WindowsLabel), func() {
-// 	DescribeTable("the process", Ordered,
-// 		func(namespace, labelName, labelValue, containerName, terminatedMessage, processName string, timeout int64) {
-// 			restartCommand := []string{"powershell", fmt.Sprintf("get-process \"%s\" | stop-process", processName)}
-// 			err := utils.CheckLivenessProbeRestartForProcess(K8sClient, Cfg, labelName, labelValue, namespace, containerName, terminatedMessage, processName, restartCommand, timeout)
-// 			Expect(err).NotTo(HaveOccurred())
+var _ = Describe("When the windows ama-logs-windows container liveness probe detects that", Ordered, Label(utils.WindowsLabel), func() {
+	DescribeTable("the process", Ordered,
+		func(namespace, labelName, labelValue, containerName, terminatedMessage, processName string, timeout int64) {
+			restartCommand := []string{"powershell", fmt.Sprintf("get-process \"%s\" | stop-process", processName)}
+			err := utils.CheckLivenessProbeRestartForProcess(K8sClient, Cfg, labelName, labelValue, namespace, containerName, terminatedMessage, processName, restartCommand, timeout)
+			Expect(err).NotTo(HaveOccurred())
 
-// 			// Wait for all processes in pod to start up before running any other tests
-// 			time.Sleep(240 * time.Second)
-// 		},
-// 		Entry("Fluentbit is not running, the container should restart", "kube-system", "component", "ama-logs-agent-windows", "ama-logs-windows",
-// 			"Fluentbit is not running", "fluent-bit", int64(120),
-// 		),
-// 		Entry("MonAgentLauncher is not running, the container should restart", "kube-system", "component", "ama-logs-agent-windows", "ama-logs-windows",
-// 			"MonAgentLauncher is not running", "MonAgentLauncher", int64(120),
-// 		),
-// 		// MonAgetnCore and others?
-// 	)
+			// Wait for all processes in pod to start up before running any other tests
+			time.Sleep(240 * time.Second)
+		},
+		Entry("Fluentbit is not running, the container should restart", "kube-system", "component", "ama-logs-agent-windows", "ama-logs-windows",
+			"Fluentbit is not running", "fluent-bit", int64(120),
+		),
+		Entry("MonAgentLauncher is not running, the container should restart", "kube-system", "component", "ama-logs-agent-windows", "ama-logs-windows",
+			"MonAgentLauncher is not running", "MonAgentLauncher", int64(120),
+		),
+		// MonAgetnCore and others?
+	)
 
-// 	It("the container-azm-ms-aks-k8scluster config for the windows daemonset has updated, the container should restart", func() {
-// 		err := utils.GetAndUpdateConfigMap(K8sClient, "container-azm-ms-aks-k8scluster", "kube-system")
-// 		Expect(err).NotTo(HaveOccurred())
-// 		err = utils.WatchForPodRestart(K8sClient, "kube-system", "component", "ama-logs-agent-windows", 300, "ama-logs-windows",
-// 			"",
-// 		)
-// 		Expect(err).NotTo(HaveOccurred())
-// 	})
-// })
+	It("the container-azm-ms-aks-k8scluster config for the windows daemonset has updated, the container should restart", func() {
+		err := utils.GetAndUpdateConfigMap(K8sClient, "container-azm-ms-aks-k8scluster", "kube-system")
+		Expect(err).NotTo(HaveOccurred())
+		err = utils.WatchForPodRestart(K8sClient, "kube-system", "component", "ama-logs-agent-windows", 300, "ama-logs-windows",
+			"",
+		)
+		Expect(err).NotTo(HaveOccurred())
+	})
+})
