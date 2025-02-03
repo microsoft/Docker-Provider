657d856749e1843995bfbb740a304d64ed2286d4
longwan <longwan@microsoft.com>
2025-02-03 20:00:15 +0000

    fix conformance test version conflict with AutoUpgradeMinorVersion

--- test/e2e/src/core/e2e_tests.sh
+++ test/e2e/src/core/e2e_tests.sh
@@ -131,9 +131,6 @@ createArcCIExtension() {
     if [ ! -z "$CI_ARC_RELEASE_TRAIN" ]; then
        basicparameters="$basicparameters  --release-train $CI_ARC_RELEASE_TRAIN"
     fi
-    if [ ! -z "$CI_ARC_VERSION" ]; then
-       basicparameters="$basicparameters  --version $CI_ARC_VERSION"
-    fi
 
    if [ ! -z "$USE_AAD_AUTH" ]; then
      az k8s-extension create $basicparameters --configuration-settings amalogs.ISTEST=true amalogs.useAADAuth=$USE_AAD_AUTH
