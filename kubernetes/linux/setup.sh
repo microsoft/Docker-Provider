28758f9a2b1432acbf8d5a82a6f362c2d8130603
Tina Ngo <ngo.t.christina@gmail.com>
2025-03-10 12:46:15 -0400

    updated ruby packages vers

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -26,29 +26,37 @@ tdnf install -y gcc patch bzip2 openssl-devel libyaml-devel libffi-devel readlin
 # rm -rf ruby-build-*
 
 # remove unused default gem openssl, find as they have some known vulns
-rm /usr/lib/ruby/gems/3.1.0/specifications/default/openssl-3.2.0.gemspec
-rm -rf /usr/lib/ruby/gems/3.1.0/gems/openssl-3.2.0
-rm /usr/lib/ruby/gems/3.1.0/specifications/default/find-0.2.0.gemspec
-rm -rf /usr/lib/ruby/gems/3.1.0/gems/find-0.2.0
-rm /usr/lib/ruby/gems/3.1.0/specifications/default/rdoc-6.6.3.1.gemspec
-rm -rf /usr/lib/ruby/gems/3.1.0/gems/rdoc-6.6.3.1
+rm /usr/lib/ruby/gems/3.3.0/specifications/default/openssl-3.2.0.gemspec
+rm -rf /usr/lib/ruby/gems/3.3.0/gems/openssl-3.2.0
+rm /usr/lib/ruby/gems/3.3.0/specifications/default/find-0.2.0.gemspec
+rm -rf /usr/lib/ruby/gems/3.3.0/gems/find-0.2.0
+rm /usr/lib/ruby/gems/3.3.0/specifications/default/rdoc-6.6.3.1.gemspec
+rm -rf /usr/lib/ruby/gems/3.3.0/gems/rdoc-6.6.3.1
 
 # update the time and uri package to tackle the vulnerabilities in these gems
 gem update time --default
 gem update uri --default
 gem update stringio --default
-gem update rexml --default
-gem update webrick --default
-mv /usr/lib/ruby/gems/3.1.0/specifications/default/time-0.2.0.gemspec /usr/lib/ruby/gems/3.1.0/specifications/default/..
-mv /usr/lib/ruby/gems/3.1.0/specifications/default/uri-0.11.0.gemspec /usr/lib/ruby/gems/3.1.0/specifications/default/..
-mv /usr/lib/ruby/gems/3.1.0/specifications/default/stringio-3.0.1.gemspec /usr/lib/ruby/gems/3.1.0/specifications/default/..
-mv /usr/lib/ruby/gems/3.1.0/specifications/default/rexml-3.2.5.gemspec /usr/lib/ruby/gems/3.1.0/specifications/default/..
-mv /usr/lib/ruby/gems/3.1.0/specifications/webrick-1.8.1.gemspec /usr/lib/ruby/gems/3.1.0/specifications/..
-gem uninstall time --version 0.2.0
-gem uninstall uri --version 0.11.0
-gem uninstall stringio --version 3.0.1
-gem uninstall rexml --version 3.2.5
-gem uninstall webrick --version 1.8.1
+
+# packages don't exist in ruby 3.3.3
+# gem update rexml --default
+# gem update webrick --default
+
+mv /usr/lib/ruby/gems/3.3.0/specifications/default/time-0.3.0.gemspec /usr/lib/ruby/gems/3.3.0/specifications/default/..
+mv /usr/lib/ruby/gems/3.3.0/specifications/default/uri-0.13.0.gemspec /usr/lib/ruby/gems/3.3.0/specifications/default/..
+mv /usr/lib/ruby/gems/3.3.0/specifications/default/stringio-3.1.1.gemspec /usr/lib/ruby/gems/3.3.0/specifications/default/..
+
+# files doesn't exist anymore in 3.3.3
+# mv /usr/lib/ruby/gems/3.3.0/specifications/default/rexml-3.2.5.gemspec /usr/lib/ruby/gems/3.1.0/specifications/default/..
+# mv /usr/lib/ruby/gems/3.3.0/specifications/webrick-1.8.1.gemspec /usr/lib/ruby/gems/3.1.0/specifications/..
+
+gem uninstall time --version 0.3.0
+gem uninstall uri --version 0.13.0
+gem uninstall stringio --version 3.1.1
+
+# packages not found in ruby 3.3.3
+# gem uninstall rexml --version 3.2.5
+# gem uninstall webrick --version 1.8.1
 
 sudo tdnf install -y azure-mdsd-1.33.3
 cp -f $TMPDIR/mdsd.xml /etc/mdsd.d
