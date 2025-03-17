60226e9a0f76c50de31d3318ff11b2d9c1fc0928
suyadav1 <87668410+suyadav1@users.noreply.github.com>
2025-03-17 12:20:58 -0700

    Test automation: added tests for LA data flow (#1366)
    * Querylogs skeleton
    
    * extract resourceId from cluster
    
    * delete cmetrics files
    
    * Added tests for table and column count
    
    * Added pod compare tests
    
    * Added testcase for nodes
    
    * Added querylogs to testkube
    
    * Added new tables for data flow check
    
    * teams channel integration
    
    * Added resource compare for all pods

---
+++ test/ginkgo-e2e/querylogs/querylogs_test.go
@@ -0,0 +1,59 @@
+package querylogs_test
+
+import (
+	"strings"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"docker-provider/test/utils"
+)
+
+var _ = Describe("When querying the logs for the table", func() {
+	DescribeTable("All tables should have logs",
+		func(table string) {
+			var err error
+			query := table + " | where TimeGenerated > ago(15m) | summarize count()"
+			err = utils.QueryLogsForCount(LogsClient, AKSResourceId, query, false)
+			// If ContainerLogV2 is configured, query ContainerLogV2 table instead of ContainerLog
+			if err != nil && strings.Contains(table, "ContainerLog") {
+				query := "ContainerLogV2 | where TimeGenerated > ago(15m) | summarize count()"
+				err = utils.QueryLogsForCount(LogsClient, AKSResourceId, query, false)
+			}
+			Expect(err).NotTo(HaveOccurred())
+		},
+		Entry("Perf", "Perf"),
+		Entry("InsightsMetrics", "InsightsMetrics"),
+		Entry("ContainerLog", "ContainerLog"),
+		Entry("ContainerInventory", "ContainerInventory"),
+		Entry("ContainerNodeInventory", "ContainerNodeInventory"),
+		Entry("KubeNodeInventory", "KubeNodeInventory"),
+		Entry("KubePodInventory", "KubePodInventory"),
+		Entry("KubePVInventory", "KubePVInventory"),
+	)
+})
+
+var _ = Describe("When querying the logs for the ContainerInventory", func() {
+	DescribeTable("Column should have zero empty values",
+		func(column string) {
+			query := "ContainerInventory | where TimeGenerated > ago(1h) | summarize countif(isempty(" + column + ") or isnull(" + column + "))"
+			err := utils.QueryLogsForCount(LogsClient, AKSResourceId, query, true)
+			Expect(err).NotTo(HaveOccurred())
+		},
+		Entry("Image", "Image"),
+		Entry("ImageID", "ImageID"),
+		Entry("ImageTag", "ImageTag"),
+		Entry("Repository", "Repository"),
+	)
+})
+
+var _ = Describe("When querying the number of resources of the cluster", func() {
+	DescribeTable("The resource from kube api should be present in logs",
+		func(table string) {
+			err := utils.CompareResourcesInLogsAndKubeAPI(K8sClient, LogsClient, AKSResourceId, table)
+			Expect(err).NotTo(HaveOccurred())
+		},
+		Entry("Pods", "KubePodInventory"),
+		Entry("Nodes", "KubeNodeInventory"),
+	)
+})
