f9ab759b602c4878e47ca570815376c7bbfa0bce
zanejohnson-azure <zanejohnson@microsoft.com>
2025-03-28 17:43:58 -0700

    move up script

--- kubernetes/windows/main.ps1
+++ kubernetes/windows/main.ps1
@@ -976,6 +976,12 @@ else {
     }
 }
 
+# Start memory monitoring script as a background job
+Write-Host "starting memory usage monitoring script"
+Start-Job -ScriptBlock {
+    Start-Process -NoNewWindow -FilePath "powershell" -ArgumentList "-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "C:\opt\amalogswindows\scripts\powershell\memoryusagemonitor.ps1"
+}
+Write-Host "finished starting memory usage monitoring script"
 
 Start-Fluent-Telegraf
 
@@ -984,10 +990,3 @@ Get-WmiObject Win32_process | Where-Object { $_.Name -match 'powershell' } | For
 
 #check if fluentd service is running
 Get-Service fluentdwinaks
-
-# Start memory monitoring script as a background job
-Write-Host "starting memory usage monitoring script"
-Start-Job -ScriptBlock {
-    Start-Process -NoNewWindow -FilePath "powershell" -ArgumentList "-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "C:\opt\amalogswindows\scripts\powershell\memoryusagemonitor.ps1"
-}
-Write-Host "finished starting memory usage monitoring script"
