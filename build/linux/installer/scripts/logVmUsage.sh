767a284f984454757bd9bc713cd55853190d4ed6
Sunil Yadav <suyadav@microsoft.com>
2025-04-07 11:28:15 -0700

    added vm usage script

---
+++ build/linux/installer/scripts/logVmUsage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Log file to store the output
+LOG_FILE="/var/log/process_vm_usage.log"
+
+# Function to log memory usage for each running process
+log_vm_usage() {
+  echo "Time: $(date)" >> "$LOG_FILE"
+  for pid in $(ps -e -o pid --no-headers); do
+    if [ -f "/proc/$pid/status" ]; then
+      command=$(ps -p $pid -o cmd --no-headers)
+      vm_usage=$(cat /proc/$pid/status | grep Vm)
+      echo "Command: $command" >> "$LOG_FILE"
+      echo "Vm Usage: $vm_usage" >> "$LOG_FILE"
+      printf "\n" >> "$LOG_FILE"
+    fi
+  done
+  echo "------------------------" >> "$LOG_FILE"
+}
+
+# Infinite loop to log every 5 minutes
+while true; do
+  log_vm_usage
+  sleep 300 # Wait for 5 minutes
+done
