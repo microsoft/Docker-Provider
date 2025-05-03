fcc58239602167f255d33f214cac245ab1cf8173
Ramanuj Bhattacharjee <rambhatt@microsoft.com>
2025-05-03 00:14:47 +0000

    done done done

---
+++ appmonitoring/scripts/prepare-aks-rp-charts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+YAML_FILE="$1"
+
+if [[ -z "$YAML_FILE" ]]; then
+  echo "Usage: $0 <yaml-file>"
+  exit 1
+fi
+
+SEARCH='image: "{{ template "addon_mcr_repository_base" . }}/azuremonitor/applicationinsights/aiprod:{{'
+REPLACE='image: "{{ template "addon_mcr_repository_base" . }}/azuremonitor/applicationinsights/aidev:{{'
+
+MATCH_COUNT=$(grep -cF "$SEARCH" "$YAML_FILE")
+
+if [[ "$MATCH_COUNT" -eq 6 ]]; then
+  sed -i "s|$SEARCH|$REPLACE|g" "$YAML_FILE"
+  echo "Replacement done in $YAML_FILE"
+elif [[ "$MATCH_COUNT" -eq 0 ]]; then
+  echo "Error: Target image line not found in $YAML_FILE"
+  exit 2
+else
+  echo "Error: Expected 6 matches, found $MATCH_COUNT in $YAML_FILE"
+  exit 3
+fi
