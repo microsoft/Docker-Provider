f68ddc97cbf4ee394ebf0d5e168c7fc0c66af4f0
suyadav1 <87668410+suyadav1@users.noreply.github.com>
2025-05-06 12:37:57 -0700

    Telegraf dalec build (#1420)

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -79,10 +79,10 @@ sudo tdnf install jq-1.6-1.cm2 -y
 #used to setcaps for ruby process to read /proc/env
 sudo tdnf install libcap -y
 
-sudo tdnf install telegraf-1.29.4 -y
+sudo tdnf install telegraf-agent-1.34.2 -y
 telegraf_version=$(sudo tdnf list installed | grep telegraf | awk '{print $2}')
 echo "telegraf $telegraf_version" >> packages_version.txt
-mv /usr/bin/telegraf /opt/telegraf
+mv /usr/bin/telegraf-agent /opt/telegraf
 
 # Use wildcard version so that it doesnt require to touch this file
 /$TMPDIR/docker-cimprov-*.*.*-*.*.sh --install
