0b7091a2ded34add81d552b34c5d6809471a5fb8
Alexander Kaplan <alkaplan@microsoft.com>
2025-05-24 19:37:46 -0700

    Update ESLint to the latest version (#1431)

--- appmonitoring/ts/src/build.cmd
+++ appmonitoring/ts/src/build.cmd
@@ -6,7 +6,7 @@ if exist "./out" (
 call npm i
 call npm ci
 
-call tsc --build || echo Build failed && exit /b
+call tsc --build --force || echo Build failed && exit /b
 call npx eslint . || echo ESLint failed && exit /b 
 call npm test || echo Jest failed && exit /b
 
