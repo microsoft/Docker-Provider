27d6e26177197269c0a1249a95178f6d65cf98ce
Sunil Yadav <suyadav@microsoft.com>
2025-05-27 12:38:56 -0700

    Upgraded fluent-bit windows to 4.0.1

--- kubernetes/windows/setup.ps1
+++ kubernetes/windows/setup.ps1
@@ -26,9 +26,9 @@ Write-Host ('Creating folder structure')
 Write-Host ('Installing Fluent Bit');
 
     try {
-        $fluentBitUri='https://releases.fluentbit.io/4.0/source-4.0.2.tar.gz'
-        Invoke-WebRequest -Uri $fluentBitUri -OutFile /installation/fluent-bit.tar.gz
-        Expand-Archive -Path /installation/fluent-bit.tar.gz -Destination /installation/fluent-bit
+        $fluentBitUri='https://packages.fluentbit.io/windows/fluent-bit-4.0.1-win64.zip'
+        Invoke-WebRequest -Uri $fluentBitUri -OutFile /installation/fluent-bit.zip
+        Expand-Archive -Path /installation/fluent-bit.zip -Destination /installation/fluent-bit
         Move-Item -Path /installation/fluent-bit/*/* -Destination /opt/fluent-bit/ -ErrorAction SilentlyContinue
     }
     catch {
