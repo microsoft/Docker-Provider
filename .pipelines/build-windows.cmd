71d41d89520102def3e673490fbc847795a71ad2
Alexander Kaplan <alkaplan@microsoft.com>
2025-06-03 16:47:15 -0700

    Clean up - remove CI code (#1462)
    * Removing all CI code
    
    * Restoring deployment files
    
    * Restore pushWebhookToAcr.sh
    
    * Removing unused steps from the build pipeline

--- .pipelines/build-windows.cmd
+++
@@ -1,4 +0,0 @@
-setlocal enabledelayedexpansion
-powershell.exe -ExecutionPolicy Unrestricted -NoProfile -WindowStyle Hidden -File "%~dp0..\build\windows\Makefile.ps1"
-endlocal
-exit /B %ERRORLEVEL%
