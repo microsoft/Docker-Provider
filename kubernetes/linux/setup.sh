ab5345e7a3cc9c19e0681c5ccafd2960cb7ca89c
Sunil Yadav <suyadav@microsoft.com>
2025-06-18 13:09:16 -0700

    upgrade telegraf 1.35.0

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -56,7 +56,8 @@ sudo tdnf install jq-1.7.1-1.azl3 -y
 #used to setcaps for ruby process to read /proc/env
 sudo tdnf install libcap -y
 
-sudo tdnf install telegraf-agent-1.34.3 -y
+sudo curl -L -O https://github.com/microsoft/Docker-Provider/releases/download/mdsd-1.31.0/telegraf-agent-1.35.0-1.azl3.x86_64.rpm
+sudo tdnf install -y --nogpgcheck telegraf-agent-1.35.0-1.azl3.x86_64.rpm
 telegraf_version=$(sudo tdnf list installed | grep telegraf | awk '{print $2}')
 echo "telegraf $telegraf_version" >> packages_version.txt
 mv /usr/bin/telegraf-agent /opt/telegraf
