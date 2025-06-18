9081b119ea858628ea92a76d71e88cd59be5a32b
Long Wan <longwan@microsoft.com>
2025-06-17 17:57:13 -0700

    Longw/high scale terraform (#1488)
    * terraform template for high log scale and networkflow logs

--- scripts/onboarding/aks/onboarding-msi-terraform/variables.tf
+++ scripts/onboarding/aks/onboarding-msi-terraform/variables.tf
@@ -1,50 +1,57 @@
 variable "agent_count" {
+  type    = number
   default = 3
 }
 
 variable "vm_size" {
-  type = string
+  type    = string
   default = "Standard_D2_v2"
 }
 
 variable "identity_type" {
-  type = string
+  type    = string
   default = "SystemAssigned"
 }
 
 variable "aks_resource_group_name" {
-  type = string
+  type    = string
   default = "<ResourceGroup>"
 }
 
 variable "resource_group_location" {
-  type = string
+  type    = string
   default = "<ResourceGroupLocation>"
 }
 
 variable "cluster_name" {
-  type = string
+  type    = string
   default = "<ClusterName>"
 }
 
+variable "cluster_location" {
+  type    = string
+  default = "<ClusterLocation>"
+}
+
 variable "dns_prefix" {
+  type    = string
   default = "k8stest"
 }
 
 variable "workspace_resource_id" {
-  type = string
+  type    = string
   default = "/subscriptions/<SubscriptionId>/resourceGroups/<ResourceGroup>/providers/Microsoft.OperationalInsights/workspaces/<workspaceName>"
 }
 
 variable "workspace_region" {
-  type = string
+  type    = string
   default = "<workspaceRegion>"
 }
 
 variable "resource_tag_values" {
   description = "Resource Tag Values"
-  type = map(string)
-  default = {
+  type        = map(string)
+  default     = {
     "<existingOrnew-tag-name1>" = "<existingOrnew-tag-value1>"
     "<existingOrnew-tag-name2>" = "<existingOrnew-tag-value2>"
     "<existingOrnew-tag-name3>" = "<existingOrnew-tag-value3>"
@@ -52,21 +59,39 @@ variable "resource_tag_values" {
 }
 
 variable "data_collection_interval" {
+  type    = string
   default = "1m"
 }
 
 variable "namespace_filtering_mode_for_data_collection" {
+  type    = string
   default = "Off"
 }
 
 variable "namespaces_for_data_collection" {
+  type    = list(string)
   default = ["kube-system", "gatekeeper-system", "azure-arc"]
 }
 
 variable "enableContainerLogV2" {
+  type    = bool
   default = true
 }
 
 variable "streams" {
- default = ["Microsoft-ContainerLog", "Microsoft-ContainerLogV2", "Microsoft-KubeEvents", "Microsoft-KubePodInventory", "Microsoft-KubeNodeInventory", "Microsoft-KubePVInventory","Microsoft-KubeServices", "Microsoft-KubeMonAgentEvents", "Microsoft-InsightsMetrics", "Microsoft-ContainerInventory",  "Microsoft-ContainerNodeInventory", "Microsoft-Perf"]
+  type    = list(string)
+  default = [
+    "Microsoft-ContainerLog",
+    "Microsoft-ContainerLogV2",
+    "Microsoft-KubeEvents",
+    "Microsoft-KubePodInventory",
+    "Microsoft-KubeNodeInventory",
+    "Microsoft-KubePVInventory",
+    "Microsoft-KubeServices",
+    "Microsoft-KubeMonAgentEvents",
+    "Microsoft-InsightsMetrics",
+    "Microsoft-ContainerInventory",
+    "Microsoft-ContainerNodeInventory",
+    "Microsoft-Perf"
+  ]
 }
