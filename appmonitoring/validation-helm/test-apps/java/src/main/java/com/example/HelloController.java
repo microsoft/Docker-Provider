0839a9c01db59f17db00c9eb279254d91aadefbd
alkaplan <alkaplan@microsoft.com>
2025-09-22 16:08:49 -0700

    feat: integrate OpenTelemetry metrics in Java test app and add configuration class

--- appmonitoring/validation-helm/test-apps/java/src/main/java/com/example/HelloController.java
+++ appmonitoring/validation-helm/test-apps/java/src/main/java/com/example/HelloController.java
@@ -6,22 +6,42 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//import io.micrometer.core.instrument.Counter;
+//import io.micrometer.core.instrument.MeterRegistry;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
 
 import java.util.Random;
 
 @RestController
 public class HelloController {
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
-    @Value("${TARGET_URL:}")
-    private String targetUrl;
+    Meter meter = GlobalOpenTelemetry.getMeter("OTEL.AzureMonitor.Demo");
+    private String targetUrl = "https://bing.com";
 
     private final RestTemplate restTemplate = new RestTemplate();
     private final Random random = new Random();
+    private final LongCounter cowsSoldCounter;
+
+    public HelloController(/*MeterRegistry meterRegistry*/) {
+        this.cowsSoldCounter = meter
+                .counterBuilder("cows_sold_total")
+                .build();
+    }
 
     @GetMapping("/")
     public String hello() {
         logger.info("Received request at root endpoint '/'");
         logger.debug("Responding with static hello message");
+        
+        // Increment cows sold counter
+        cowsSoldCounter.add(1, Attributes.of(AttributeKey.stringKey("name"), "cow", AttributeKey.stringKey("color"), "white"));
+        logger.debug("Incremented cows_sold_total metric");
+        
         return "Hello from Java test app!";
     }
 
@@ -29,6 +49,12 @@ public class HelloController {
     public String callTarget() {
         logger.info("Received request at '/call-target' endpoint");
         logger.debug("TARGET_URL value: {}", targetUrl);
+        
+        // Increment cows sold counter
+        cowsSoldCounter.add(1, Attributes.of(AttributeKey.stringKey("name"), "cow", AttributeKey.stringKey("color"), "white"));
+        
+        logger.debug("Incremented cows_sold_total metric");
+        
         // Occasionally throw an error
         if (random.nextInt(10) >= 7) { // 20% chance
             logger.error("Simulated error at '/call-target' endpoint");
