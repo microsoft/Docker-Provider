aee2203224d7764ba81630acaeff4a0830b9a266
longwan <longwan@microsoft.com>
2025-09-30 06:00:53 +0000

    test add postgresql

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -71,6 +71,7 @@ echo "DOCKER_CIMPROV_VERSION=$docker_cimprov_version" >> packages_version.txt
 
 #install fluent-bit
 sudo tdnf install azcu-fluent-bit-4.0.9 -y
+sudo tdnf install postgresql-libs -y  # Add PostgreSQL dependencies for fluent-bit
 echo "$(fluent-bit --version)" >> packages_version.txt
 
 # install fluentd
