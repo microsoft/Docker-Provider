3af73a002c9d886615c06e6373c345c19f110373
rambhatt <rambhatt@microsoft.com>
2025-10-09 21:50:04 +0000

    add changes

--- appmonitoring/scripts/install-oci-test-apps.sh
+++ appmonitoring/scripts/install-oci-test-apps.sh
@@ -18,22 +18,30 @@ echo "Wait for 10s for CR to be applied and picked up..."
 sleep 10
 
 # ACR and chart version
-ACR_NAME="aicommon.azurecr.io"
+ACR_NAME="appmonitoring.azurecr.io"
 CHART_VERSION="0.1.0"
-
 echo "Installing test apps from OCI registry..."
 
 # This app will be called by all the instrumented test apps to generate dependency telemetry for us to test
 echo "Installing testappsource..."
-helm install testappsource oci://${ACR_NAME}/helm/testapps/testappsource --version ${CHART_VERSION} -n ${TEST_NS}
+if ! helm install testappsource oci://${ACR_NAME}/helm/testapps/testappsource --version ${CHART_VERSION} -n ${TEST_NS}; then
+  echo "Error: testappsource installation failed"
+  exit 1
+fi
 
 # this is the instrumented java app
 echo "Installing java-test-app..."
-helm install java-test-app oci://${ACR_NAME}/helm/testapps/java-test-app --version ${CHART_VERSION} -n ${TEST_NS}
+if ! helm install java-test-app oci://${ACR_NAME}/helm/testapps/java-test-app --version ${CHART_VERSION} -n ${TEST_NS}; then
+  echo "Error: java-test-app installation failed"
+  exit 1
+fi
 
 # this is the instrumented nodejs app
 echo "Installing nodejs-test-app..."
-helm install nodejs-test-app oci://${ACR_NAME}/helm/testapps/nodejs-test-app --version ${CHART_VERSION} -n ${TEST_NS}
+if ! helm install nodejs-test-app oci://${ACR_NAME}/helm/testapps/nodejs-test-app --version ${CHART_VERSION} -n ${TEST_NS}; then
+  echo "Error: nodejs-test-app installation failed"
+  exit 1
+fi
 
 # this is the instrumented python app (if available, skip if not)
 echo "Installing python-test-app..."
@@ -41,10 +49,16 @@ helm install python-test-app oci://${ACR_NAME}/helm/testapps/python-test-app --v
 
 # this is the instrumented dotnet app
 echo "Installing dotnet-test-app..."
-helm install dotnet-test-app oci://${ACR_NAME}/helm/testapps/dotnet-test-app --version ${CHART_VERSION} -n ${TEST_NS}
+if ! helm install dotnet-test-app oci://${ACR_NAME}/helm/testapps/dotnet-test-app --version ${CHART_VERSION} -n ${TEST_NS}; then
+  echo "Error: dotnet-test-app installation failed"
+  exit 1
+fi
 
 # this is the app that will periodically call the instrumented apps to generate request telemetry
 echo "Installing testappcaller..."
-helm install testappcaller oci://${ACR_NAME}/helm/testapps/testappcaller --version ${CHART_VERSION} -n ${TEST_NS}
+if ! helm install testappcaller oci://${ACR_NAME}/helm/testapps/testappcaller --version ${CHART_VERSION} -n ${TEST_NS}; then
+  echo "Error: testappcaller installation failed"
+  exit 1
+fi
 
 echo "All test apps installed successfully!"
