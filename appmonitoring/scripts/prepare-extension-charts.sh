7674b0b110ab0a31465a2c51b8d85e76ecb0344c
rambhatt <rambhatt@microsoft.com>
2025-10-13 23:01:08 +0000

    fix: update image tag handling in pipeline and chart preparation scripts

--- appmonitoring/scripts/prepare-extension-charts.sh
+++ appmonitoring/scripts/prepare-extension-charts.sh
@@ -2,6 +2,7 @@
 
 CHART_DIR="$1"
 IMAGE_TAG="$2"
+OCI_TAG="$3"
 
 if [[ -z "$CHART_DIR" ]] || [[ -z "$IMAGE_TAG" ]]; then
   echo "Usage: $0 <chart-directory> <image-tag>"
@@ -12,11 +13,11 @@ echo "Preparing chart in directory: $CHART_DIR"
 echo "Using image tag: $IMAGE_TAG"
 
 # Replace <CHART_VERSION> in Chart.yaml
-sed -i "s|<CHART_VERSION>|$IMAGE_TAG|g" "$CHART_DIR/Chart.yaml"
-echo "Replaced <CHART_VERSION> in Chart.yaml"
+sed -i "s|<CHART_VERSION>|$OCI_TAG|g" "$CHART_DIR/Chart.yaml"
+echo "Replaced <CHART_VERSION> in Chart.yaml with $OCI_TAG"
 
 # Replace <IMAGE_TAG> in values.yaml
 sed -i "s|<IMAGE_TAG>|$IMAGE_TAG|g" "$CHART_DIR/values.yaml"
-echo "Replaced <IMAGE_TAG> in values.yaml"
+echo "Replaced <IMAGE_TAG> in values.yaml with $IMAGE_TAG"
 
 echo "Chart preparation complete"
