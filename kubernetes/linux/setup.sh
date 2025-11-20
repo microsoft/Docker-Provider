c6d0c403474410cecc1f8dd0202e58b2c87e6c39
zanejohnson-azure <zanejohnson@microsoft.com>
2025-11-20 14:55:13 -0800

    fluentd install on linux fix

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -84,6 +84,13 @@ gem install fluentd -v $fluentd_version --no-document
 rm -rf /usr/lib/ruby/gems/3.3.0/gems/fluentd-$fluentd_version/test/
 
 echo "$(fluentd --version)" >> packages_version.txt
+
+# Create symlink for fluentd to /usr/bin for Docker COPY command
+fluentd_path=$(which fluentd)
+echo "Fluentd installed at: $fluentd_path"
+ln -s $fluentd_path /usr/bin/fluentd
+echo "Created symlink: /usr/bin/fluentd -> $fluentd_path"
+
 fluentd --setup ./fluent
 
 gem install gyoku iso8601 bigdecimal --no-doc
