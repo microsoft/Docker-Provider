35b60fd813d14885429de4dd7e0c6b656a721da4
zanejohnson-azure <zanejohnson@microsoft.com>
2026-01-22 13:57:48 -0800

    pull change from ci_prod

--- test/testkube/install-and-execute-testkube-tests.sh
+++ test/testkube/install-and-execute-testkube-tests.sh
@@ -70,7 +70,7 @@ kubectl apply -f ./api-server-permissions.yaml
 kubectl apply -f ./testkube-test-crs.yaml
 
 echo "Wait for cluster to be ready"
-sleep 200
+sleep 300
 
 echo "Run testkube testworkflows"
 workflows=()
